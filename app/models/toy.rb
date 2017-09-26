870f020b410efa47b172107dda0a9cd3dfef78b0
Okan Binli <okanb3@gmail.com>
2017-09-26 15:46:09 +0300

    update toy medium thumbnail size

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,7 +1,7 @@
 class Toy < ApplicationRecord
   belongs_to :user
 
-  has_attached_file :image, styles: { large: "700x700", medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { large: "700x700", medium: "x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   enum toy_type: [:akülü_araç, :okul_öncesi, :bebek, :figür, :lego, :kostüm, :dış_mekan]
