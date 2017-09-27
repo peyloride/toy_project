d6a93d60b6136ceee972f60bf899aaef42af774f
Okan Binli <okanb3@gmail.com>
2017-09-27 13:19:46 +0300

    lend system improved, checks for lending added

--- app/helpers/toys_helper.rb
+++ app/helpers/toys_helper.rb
@@ -1,2 +1,17 @@
 module ToysHelper
+  def is_lendable(toy)
+    @lend = Lend.where(toy_id: toy.id)
+
+    if @lend.present?
+      if @lend.last.created_at < 1.month.ago && toy.user_id != current_user.id
+        return true
+      else
+        return false
+      end
+    else
+      return true
+    end
+
+    return false
+  end
 end
