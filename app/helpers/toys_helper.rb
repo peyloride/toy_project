97181acfe7b140ca6b9c85f607816386a49e1c2e
Okan Binli <okanb3@gmail.com>
2017-09-27 16:42:05 +0300

    toy lend system completed

--- app/helpers/toys_helper.rb
+++ app/helpers/toys_helper.rb
@@ -3,15 +3,16 @@ module ToysHelper
     @lend = Lend.where(toy_id: toy.id)
 
     if @lend.present?
-      if @lend.last.created_at < 1.month.ago && toy.user_id != current_user.id
+      if (@lend.last.created_at < 1.month.ago) && (toy.user_id != current_user.id)
         return true
       else
         return false
       end
     else
-      return true
+      if toy.user_id != current_user.id
+        return true
+      end
     end
-
     return false
   end
 end
