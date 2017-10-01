08da88a17a3879f0d2ec8e66aff2001d1f5f9575
Okan Binli <okanb3@gmail.com>
2017-10-01 15:40:43 +0300

    refactor routes according to new controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :toys do
-    post '/lend', to: 'toys#lend'
+    post '/request_lend', to: 'lends#lend_request'
+    post '/accept_lend', to: 'lends#accept'
+    post '/refuse_lend', to: 'lends#refuse'
   end
+  get '/my_lends', to: 'lends#my_lends'
   get '/my_toys', to: 'toys#my_toys', as: 'my_toys'
 end
