c49d016db6ead61337d5eaf8a72c77034e6ad96e
Okan Binli <okanb3@gmail.com>
2017-10-03 15:02:26 +0300

    lend has 2 relation to user, owner and lender

--- app/models/lend.rb
+++ app/models/lend.rb
@@ -1,4 +1,5 @@
 class Lend < ApplicationRecord
-  belongs_to :user, class_name: 'User',foreign_key: :owner_id
+  belongs_to :owner, class_name: 'User', foreign_key: :owner_id
+  belongs_to :lender, class_name: 'User', foreign_key: :borrower_id
   belongs_to :toy
 end
