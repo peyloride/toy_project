8706d3c8c4ca95837e8fb54b4a0be71a4f0ede2d
Okan Binli <okanb3@gmail.com>
2017-10-04 14:39:20 +0300

    comments added to toys and they are likeable #2

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,41 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_toy
+
+  def new
+    @comment = Comment.new
+  end
+
+  def like
+    @comment = Comment.find(params[:comment_id])
+    current_user.likes @comment
+    redirect_back(fallback_location: root_path)
+  end
+
+  def create
+    @comment = @toy.comments.new comment_params
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      redirect_back(fallback_location: root_path, notice: 'Your comment was successfully posted!')
+    else
+      redirect_back(fallback_location: root_path, notice: "Your comment wasn't posted!")
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to toy_path(@toy), notice: "Toy deleted successfully"
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def find_toy
+    @toy = Toy.find_by_id(params[:toy_id])
+  end
+end
